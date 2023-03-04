2498a95c813111f6a7b28f1dc75e6734568b0185
mia <mia@aider.ai>
2023-03-05 00:01:20 +1300

    chore: Initialisation + TS Replacements

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+const App: React.FC = () => {
+  
+  return(
+    <div className="myApp">
+      This is new Demo
+    </div>
+  )
+}
+
+export default App
